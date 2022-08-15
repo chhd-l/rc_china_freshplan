11328286669a44e35babb4f873edd5f4d888da2f
chhd <xscnwan13@163.com>
2022-08-15 11:05:12 +0800

    首页

--- src/pages/subscription/index.tsx
+++ src/pages/subscription/index.tsx
@@ -1,8 +1,40 @@
-import { View } from '@tarojs/components'
+import { Swiper, SwiperItem, View } from '@tarojs/components'
+import { useState } from 'react'
 import './index.less'
+import { titleSwiperList } from './index.module'
 
 const Subscription = () => {
-  return <View className="subscription-intrduce">index</View>
+  const [current, setCurrent] = useState(0)
+  return (
+    <View className="subscription">
+      <View className="TitleSwiper relative">
+        <Swiper
+          current={current}
+          className="h-full"
+          circular
+          autoplay
+          interval={2000}
+          onChange={(e) => setCurrent(e.detail.current)}
+        >
+          {/* {titleSwiperList.map((_, key) => (
+            <SwiperItem key={key} className="bg-red-500">
+              xxxxx
+            </SwiperItem>
+          ))} */}
+          <SwiperItem key="swiper-item-{{index}}" className="bg-red-500">
+            xxxxx
+          </SwiperItem>
+        </Swiper>
+        <View className="indicatorDots absolute w-full">
+          <View className="flex items-center justify-center">
+            {titleSwiperList.map((_, key) => (
+              <View key={key} className={`${current === key && 'selectInd'} rounded-full`} />
+            ))}
+          </View>
+        </View>
+      </View>
+    </View>
+  )
 }
 
 export default Subscription
