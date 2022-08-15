9e3fa12f3ce3ae82b81c4ca241e007cc23703b7b
chhd <xscnwan13@163.com>
2022-08-15 09:45:21 +0800

    upd：头像放大

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -21,7 +21,7 @@ const Account = () => {
   const [consumer, setConsumer] = useAtom(consumerAtom)
 
   const loginInit = async () => {
-    if (my.getStorageSync({ key: 'wxLoginRes'})) {
+    if (my.getStorageSync({ key: 'wxLoginRes' })) {
       // Taro.setStorageSync('commerce-loading', 1)
       const data = await wxLogin()
       setConsumer(data)
@@ -47,7 +47,7 @@ const Account = () => {
   return (
     <View className="Account pb-2">
       <View className="flex items-center loginHerder">
-        <AtAvatar className="mx-1.5" circle image={consumer?.avatarUrl || 'https://jdc.jd.com/img/200'} />
+        <AtAvatar size="large" className="mx-1.5" circle image={consumer?.avatarUrl || 'https://jdc.jd.com/img/200'} />
         {consumer?.id ? (
           <View className="flex flex-col">
             <Text className="UserName">{consumer?.nickName}</Text>
