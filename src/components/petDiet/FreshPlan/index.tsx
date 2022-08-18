9a8d02ff090c1d42e97579d22397af74097af03d
tutu <1515112287@qq.com>
2022-08-18 14:28:36 +0800

    PetDiet

---
+++ src/components/petDiet/FreshPlan/index.tsx
@@ -0,0 +1,11 @@
+import { View } from '@tarojs/components'
+
+const FreshPlan = () => {
+  return (
+    <View>
+      <View className="text-[64px] font-bold">Fresh Plan </View>
+    </View>
+  )
+}
+
+export default FreshPlan
