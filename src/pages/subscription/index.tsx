e789af0019492d7613f403a92b814f86cc87c059
chhd <xscnwan13@163.com>
2022-08-18 15:01:29 +0800

    首页新老用户有无计划展示不同页面

--- src/pages/subscription/index.tsx
+++ src/pages/subscription/index.tsx
@@ -1,8 +1,9 @@
-import Planned from '@/components/Planned'
 import NoPlan from '@/components/NoPlan'
+import Planned from '@/components/Planned'
 import { getSubscriptionFindByConsumerId } from '@/framework/api/subscription/subscription'
+import { getAgeYear } from '@/utils/utils'
 import Taro from '@tarojs/taro'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 const Index = () => {
   const [SubscriptionList, setSubscriptionList] = useState([])
@@ -10,18 +11,23 @@ const Index = () => {
 
   const getSubscriptionList = async () => {
     const res = await getSubscriptionFindByConsumerId()
-    console.log('res', res)
+    res.forEach((item) => {
+      item.pet.age = getAgeYear(item.pet.birthday)
+    })
     setSubscriptionList(res)
   }
 
-  useEffect(() => {
+  Taro.useDidShow(() => {
     let wxLoginRes = Taro.getStorageSync('wxLoginRes')
-    console.log('wxLoginRes', wxLoginRes)
     getSubscriptionList()
     setConsumer(wxLoginRes)
-  }, [])
+  })
 
-  return consumer?.userInfo?.id && SubscriptionList.length ? <Planned /> : <NoPlan />
+  return consumer?.userInfo?.id && SubscriptionList.length ? (
+    <Planned subscriptionList={SubscriptionList} />
+  ) : (
+    <NoPlan />
+  )
 }
 
 export default Index
