d3a0c3c813ebf960f0a97a1cdee8d83a4515d65b
chhd <xscnwan13@163.com>
2022-09-05 16:13:53 +0800

    修改样式

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,8 +2,8 @@
 
 export default defineAppConfig({
   pages: [
-    'pages/account/index',
     'pages/subscription/index',
+    'pages/account/index',
     'pages/foodRecom/index',
     'pages/freshPlanDetails/index',
     'pages/schedule/index',
